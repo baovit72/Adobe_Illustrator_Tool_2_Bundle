6c17e4762809d869200036a1296a830ce9829403
Ho Bao <BAO HO>
2019-12-30 15:37:50 +0700

    Complete export 4 files

---
+++ Demo.jsx
@@ -0,0 +1,9 @@
+﻿ var doc = app.documents.add(
+            DocumentColorSpace.RGB,
+            200,
+            200,
+            6,
+            DocumentArtboardLayout.GridByCol,
+            20,
+           2
+        );
